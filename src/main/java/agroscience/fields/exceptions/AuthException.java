50cc99f5e780cfe2bf9162af963c60631de5ad38
FergeSS <sasha-uhnov@yandex.ru>
2024-12-24 03:26:40 +0300

    1

---
+++ src/main/java/agroscience/fields/exceptions/AuthException.java
@@ -0,0 +1,16 @@
+package agroscience.fields.exceptions;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthException extends RuntimeException {
+
+  private final String message;
+
+  public AuthException(String message) {
+    this.message = message;
+  }
+
+}
